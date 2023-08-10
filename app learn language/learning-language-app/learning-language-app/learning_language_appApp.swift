f611a4e1639488634025a9930b7e7df3479c0bd0
Christinne <vanessa0christinne@gmail.com>
2023-08-10 16:45:12 +0200

    Update

---
+++ app learn language/learning-language-app/learning-language-app/learning_language_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_language_appApp.swift
+//  learning-language-app
+//
+//  Created by Vanessa Christinne on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct learning_language_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
