e88a0da09650d0169d9d7c9d2ed99d8db4f9f8f4
Christinne <vanessa0christinne@gmail.com>
2023-08-21 11:01:05 +0200

    update learn app

--- app learn language/learning-language-app/learning-language-app/ContentView.swift
+++ app learn language/learning-language-app/learning-language-app/ContentView.swift
@@ -1,19 +1,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+	var body: some View {
+		
+		BarLeftView()
+		
     }
 }
+
+
+// MARK: - LEFT BAR
+private struct BarLeftView: View {
+	
+	var body: some View {
+		
+		VStack {
+			Image("app icon rose")
+				.resizable()
+				.cornerRadius(90)
+				.frame(width: 60, height: 60)
+				.position(
+					x: 60,
+					y: 50)
+			
+			Image(systemName: "barometer")
+				.resizable()
+				.frame(width: 25, height: 25)
+				.position(x: 50, y: -50)
+		}
+		
+	}
+	
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+			.previewInterfaceOrientation(.landscapeRight)
     }
 }
